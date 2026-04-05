c3e41b4780a782ce2989431dfe4114c3ceaf2eb6
Ashenafi Damena <ashenafidamena415@gmail.com>
2026-04-05 14:00:00 +0300

    feat: tweak Arguments example (Ashenafi)

---
+++ Ashenafi-Damena/module-1/Test-code/SOLIDITY FUNCTIONS/Arguments.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "forge-std/Test.sol";
+import "../src/Contract.sol";
+
+contract ContractTest is Test {
+    Contract public myContract;
+
+    function setUp() public {
+        myContract = new Contract(5);
+    }
+
+    function testConstructor() public {
+        assertEq(myContract.x(), 5);
+    }
+}
+
+// Note: small args example tweak by Ashenafi
